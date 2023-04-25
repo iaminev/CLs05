59ef216aa110ef6cd4580c30032702b8d0d485fe
Ilshat Aminev <i.aminev@gmail.com>
2023-04-25 09:15:55 +0300

    Task 34

---
+++ Task 34/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintArray(int[] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+        System.Console.Write($"{matr[i]} ");
+}
+
+int[] GetRandomArray(
+    int length = 12,
+    int startPosition = 100,
+    int finishPosition = 999)
+{
+    int[] array = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(startPosition, finishPosition + 1);
+    }
+    return array;
+}
+
+
+int CountEvenNumbersInArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] myArray = GetRandomArray(16, 100, 999);
+PrintArray(myArray);
+System.Console.Write($" -> {CountEvenNumbersInArray(myArray)}");
+
